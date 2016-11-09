61bbb845b2f1fdd6abe86c461108068aeea66c92
Per Guth <mail@perguth.de>
2016-11-09 14:25:03 +0100

    lang switcher for web frontend

--- browser.js
+++ browser.js
@@ -2,14 +2,25 @@ const Clipboard = require('clipboard')
 const x = require('./lib')
 
 window.getTreeName = getTreeName
+window.setLanguage = setLanguage
+
+var lang = window.localStorage.getItem('language') || ''
+
+function setLanguage (e) {
+  lang = e.target.options[e.target.selectedIndex].value
+  window.localStorage.setItem('language', lang)
+}
 
 function getTreeName (e) {
   var textField = document.getElementsByTagName('input')[0]
-  textField.value = x.random()
-  if (e) textField.select()
+  textField.value = x.random(lang)
+  // if (e) textField.select()
 }
 
 document.addEventListener('DOMContentLoaded', function () {
+  if (lang) {
+    document.querySelector(`select [value=${lang}]`).selected = true
+  }
   new Clipboard('.btn') // eslint-disable-line
-  getTreeName()
+  getTreeName(lang)
 }, false)
