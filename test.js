31a286c3c642bb2115dacc06071ecccc08ac7be9
Per Guth <mail@perguth.de>
2016-11-10 14:56:45 +0100

    alphabetical ordering; tests

--- test.js
+++ test.js
@@ -2,9 +2,11 @@ var test = require('tape')
 var x = require('./')
 
 test('all the things', t => {
-  t.plan(4)
+  t.plan(6)
   t.ok(x.all.length > 0, 'Got tree names.')
   t.ok(x.random(), 'Got a random tree name.')
+  t.ok(x.random('en'), 'Got a random english tree name.')
   t.notEqual(x.random(), x.random(), 'Got different tree names on consecutive tries.')
-  t.equal(x.all[0], 'abendlaendischer-lebensbaum', 'First tree name matches tree list.')
+  t.equal(x.all('de')[0], 'abendlaendischer-lebensbaum', 'First german tree name matches tree list.')
+  t.equal(x.all('en')[0], 'acoma-crapemyrtle', 'First english tree name matches tree list.')
 })
